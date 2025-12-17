77affbec146086391d3d517740fa1d978252ecbd
VIDYANKSHINI <vibhutevidyankshini8@gmail.com>
2025-12-17 16:36:00 +0530

    updated reports

--- src/components/reports.tsx
+++ src/components/reports.tsx
@@ -34,7 +34,7 @@ export function Reports({ onNavigate }: ReportsProps) {
     {
       batchId: 'SW-2846',
       date: '12 Dec 2024',
-      time: '09:15 AM',
+      time: '11:15 AM',
       totalPads: 50,
       passed: 45,
       failed: 5,
